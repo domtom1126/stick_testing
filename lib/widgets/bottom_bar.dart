ea8ad82a239f14c4f8dc7518c440d78a1a5eb5b2
dom thomas <domtom1126@gmail.com>
2022-01-13 20:43:08 -0500

    Bottom nav bar widget redirects to the homw screen

---
+++ lib/widgets/bottom_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:testing/screens/home.dart';
+import 'package:testing/screens/post.dart';
+import 'package:testing/screens/profile.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  final List<Widget> _children = [
+    const Home(),
+    const Post(),
+    const Profile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
