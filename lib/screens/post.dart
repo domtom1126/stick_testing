dd293150f64633f6f19067fe6fab2cdd579eff6d
dom thomas <domtom1126@gmail.com>
2022-01-16 00:45:47 -0500

    Seperated widgets onto form_widgets

--- lib/screens/post.dart
+++ lib/screens/post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testing/widgets/form_widgets/form_widgets.dart';
 
 class Post extends StatefulWidget {
   const Post({Key? key}) : super(key: key);
@@ -7,26 +8,16 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
-  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Post a Car'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Text('Make'),
-            TextFormField(),
-            Text('Model'),
-            TextFormField(),
-            Text('Year'),
-            TextFormField(),
-            Text('Odometer'),
-            TextFormField(),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Container(
+          child: formWidget(),
         ),
       ),
     );
