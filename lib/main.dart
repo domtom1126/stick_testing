bbfd19382ee97f2d06c821eb852809ed31c2507e
dom thomas <domtom1126@gmail.com>
2022-01-18 00:28:46 -0500

    Added firebase initializer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:testing/screens/post.dart';
 import 'package:testing/screens/profile.dart';
 import 'package:testing/widgets/bottom_bar.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
