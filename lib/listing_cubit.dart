47a60e99205e210ecb85f82f7b9eb8f12689b3e5
dom thomas <domtom1126@gmail.com>
2022-01-19 22:51:12 -0500

    Changed functionality, instead of adding the cars this retrieves the cars from firebase

--- lib/listing_cubit.dart
+++ lib/listing_cubit.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:testing/listing.dart';
 
+import 'firebase_functions/get_listings.dart';
 import 'firebase_functions/post_listing.dart';
 
-class ListingCubit extends Cubit<List<NewListing>> {
-  final PostListing _postListing = PostListing();
+class ListingCubit extends Cubit<List<Listing>> {
+  final GetListing _postListing = GetListing();
   ListingCubit() : super([]);
 
-  void addListing() async => emit(await _postListing.postCar());
+  void getListing() async => emit(await _postListing.getListing());
 }
