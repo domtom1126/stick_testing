6bdc49208479a262ed8b98860acf0dd951c9b45c
dom thomas <domtom1126@gmail.com>
2022-01-19 22:52:21 -0500

    Added toJson method

--- lib/listing.dart
+++ lib/listing.dart
@@ -1,25 +1,34 @@
 // * This is like post.dart
 
-class NewListing {
-  final String make;
-  final String model;
-  final String year;
-  final int odometer;
-  final int price;
-
-  NewListing({
+class Listing {
+  Listing({
     required this.make,
     required this.model,
     required this.year,
-    required this.odometer,
     required this.price,
+    required this.odometer,
   });
 
-  factory NewListing.fromJson(Map<String, dynamic> json) => NewListing(
-        make: json['make'],
-        model: json['model'],
-        year: json['year'],
-        odometer: json['odometer'],
-        price: json['price'],
-      );
+  Listing.fromJson(Map<String, Object?> json)
+      : this(
+            make: json['make'] as String,
+            model: json['model'] as String,
+            year: json['year'] as int,
+            price: json['price'] as int,
+            odometer: json['odometer'] as int);
+  String make;
+  String model;
+  int year;
+  int price;
+  int odometer;
+
+  Map<String, Object?> toJson() {
+    return {
+      'make': make,
+      'model': model,
+      'year': year,
+      'price': price,
+      'odometer': odometer,
+    };
+  }
 }
