93676102801785973c00d1c5184bc1be6447f464
dom thomas <domtom1126@gmail.com>
2022-01-22 23:26:18 -0500

    addPost method posts to firebase

--- lib/firebase_functions/post_listing.dart
+++ lib/firebase_functions/post_listing.dart
@@ -1,19 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:testing/listing.dart';
+
 // * This would be like data_service.dart
 class PostListing {
   late final String id;
   late final String make;
+  late final String model;
+  late final String year;
+  late final String odometer;
+  late final String price;
+  late final String dateAdded;
 
+  PostListing({
+    // required this.id,
+    required this.make,
+    required this.model,
+    required this.year,
+    required this.odometer,
+    required this.price,
+    required this.dateAdded,
+  });
 
-  CollectionReference add_post =
-      FirebaseFirestore.instance.collection('posts');
-  Future<List<NewListing>> postCar() {
-    // todo this will post to firebase
-    try {
-      return add_post.add({
-
-      })
-    }
+  addPost() async {
+    CollectionReference addPost =
+        FirebaseFirestore.instance.collection('posts');
+    await addPost.add({
+      // 'id': id,
+      'make': make,
+      'model': model,
+      'year': year,
+      'odometer': odometer,
+      'price': price,
+      'date_added': dateAdded,
+    });
+    return addPost;
   }
 }
