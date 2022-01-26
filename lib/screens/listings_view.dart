c477676dea3e5332eebae4c5b2e0b63f128da7cb
dom thomas <domtom1126@gmail.com>
2022-01-25 23:45:17 -0500

    Got rid o f appbar

--- lib/screens/listings_view.dart
+++ lib/screens/listings_view.dart
@@ -1,39 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import '../listing.dart';
-import '../listing_cubit.dart';
-
-class ListingsView extends StatelessWidget {
-  ListingsView({Key? key}) : super(key: key);
+class ListingView {
   final cars = FirebaseFirestore.instance
       .collection('posts')
       .orderBy('date_added', descending: true)
       .snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Find a Stick'),
-      ),
-      body: StreamBuilder(
-        stream: cars,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-                children: snapshot.data!.docs.map((publicList) {
-              return ListTile(
-                title: Text('${publicList['make']}'),
-                subtitle: Text('${publicList['model']}'),
-                onTap: () {},
-              );
-            }).toList());
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+  Widget getListings() {
+    return StreamBuilder(
+      stream: cars,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          return ListView(
+              children: snapshot.data!.docs.map((publicList) {
+            return ListTile(
+              title: Text('${publicList['make']}'),
+              subtitle: Text('${publicList['model']}'),
+              onTap: () {},
+            );
+          }).toList());
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
