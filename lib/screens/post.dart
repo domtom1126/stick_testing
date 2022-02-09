7961781a1fc4225e10562b6a34a4e71d9f0645a5
dom thomas <domtom1126@gmail.com>
2022-02-08 21:58:49 -0500

    changed inputs styled modal

--- lib/screens/post.dart
+++ lib/screens/post.dart
@@ -23,19 +23,25 @@ class _PostState extends State<Post> {
         child: Form(
           key: _formKey,
           child: Container(
-            height: 400,
-            decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(20))),
+            // height: 400,
+            // decoration: BoxDecoration(
+            //     border: Border.all(
+            //       color: Colors.grey,
+            //     ),
+            //     borderRadius: BorderRadius.all(Radius.circular(20))),
             child: Column(children: [
               makeInput(),
+              const SizedBox(height: 20),
               modelInput(),
+              const SizedBox(height: 20),
               yearInput(),
+              const SizedBox(height: 20),
               odometerInput(),
+              const SizedBox(height: 20),
               priceInput(),
+              const SizedBox(height: 20),
               descriptionInput(),
+              const SizedBox(height: 20),
               ElevatedButton(
                   onPressed: () => confirmModal(),
                   child: const Text('Confirm')),
@@ -51,36 +57,45 @@ class _PostState extends State<Post> {
     setState(
       () {
         showBottomSheet(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
           context: context,
           builder: (BuildContext context) {
             return SizedBox(
               height: MediaQuery.of(context).size.height * 0.3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Are you sure you want to add this car?',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        child: const Text('Yes'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                          addCar();
-                        },
-                      ),
-                      ElevatedButton(
-                        child: const Text('No'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Are you sure you want to add this car?',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          child: const Text('Yes'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            addCar();
+                          },
+                        ),
+                        ElevatedButton(
+                          child: const Text('No'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             );
           },
