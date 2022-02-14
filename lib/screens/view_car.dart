38848cf5afbde67fba9f45f3c6528a87271706f2
dom thomas <domtom1126@gmail.com>
2022-02-14 00:14:15 -0500

    updated dialog, good code bro

--- lib/screens/view_car.dart
+++ lib/screens/view_car.dart
@@ -1,55 +1,76 @@
 import 'package:flutter/material.dart';
 
-// TODO make this a class and then add like button and email stuff here
+class ViewCar extends StatefulWidget {
+  // const ViewCar({Key? key}) : super(key: key);
+  final String make;
+  final String model;
+  final String year;
+  final String price;
+  final String odometer;
+  final String description;
 
-Widget viewCar(String make, String model, String year, String price,
-    String odometer, String description) {
-  return SizedBox(
-    height: 500,
-    child: Column(
-      // TODO change this height so its responsive
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Placeholder(
-                fallbackHeight: 250,
-              ),
-              Row(
-                children: [
-                  Text('$year ', style: const TextStyle(fontSize: 20)),
-                  Text(
-                    '$make $model',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Text(price),
-              Text('$odometer Miles'),
-              Text(description),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () => AlertDialog(
-                            title: Text('this was liked'),
-                            content: Container(
-                              child: Text('This was liked'),
-                            ),
-                          ),
-                      child: Icon(Icons.favorite)),
-                  ElevatedButton(onPressed: null, child: Icon(Icons.email)),
-                ],
-              ),
-            ],
-          ),
-        )
-      ],
-    ),
-  );
+  // TODO add images bro
+  // final String image;
+
+  const ViewCar(this.make, this.model, this.year, this.price, this.odometer,
+      this.description);
+
+  @override
+  _ViewCarState createState() => _ViewCarState();
+}
+
+class _ViewCarState extends State<ViewCar> {
+  @override
+  Widget build(BuildContext buildContext) {
+    return SizedBox(
+      height: 500,
+      child: Column(
+        // TODO change this height so its responsive
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Placeholder(
+                  fallbackHeight: 250,
+                ),
+                Row(
+                  children: [
+                    Text('${widget.year} ',
+                        style: const TextStyle(fontSize: 20)),
+                    Text(
+                      '${widget.make} ${widget.model}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Text(widget.price),
+                Text('${widget.odometer} Miles'),
+                Text(widget.description),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () => showDialog(
+                            context: context,
+                            builder: (context) => const AlertDialog(
+                                  title: Text('This car was liked'),
+                                  content: Text('This car was liked'),
+                                )),
+                        child: const Icon(Icons.favorite)),
+                    const ElevatedButton(
+                        onPressed: null, child: Icon(Icons.email)),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
