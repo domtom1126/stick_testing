63f9c56a4b98e6de6e8567858109db0ca7dddc54
dom thomas <domtom1126@gmail.com>
2022-02-15 23:31:16 -0500

    removed all the dumb widgets

--- lib/widgets/form_widgets/form_widgets.dart
+++ lib/widgets/form_widgets/form_widgets.dart
@@ -12,151 +12,138 @@ TextEditingController _odometerController = TextEditingController();
 TextEditingController _priceController = TextEditingController();
 TextEditingController _descriptionController = TextEditingController();
 
-Widget makeInput() {
+Widget inputField(
+    String label, TextEditingController controller, String errorMessage) {
   return TextFormField(
-    controller: _makeController,
-    decoration: const InputDecoration(
-      hintText: 'Make',
+    controller: controller,
+    decoration: InputDecoration(
+      hintText: label,
     ),
     validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter make (Ford, Honda, etc)';
-      }
-      return _makeController.text;
-    },
-  );
-}
-
-Widget modelInput() {
-  return TextFormField(
-    controller: _modelController,
-    decoration: const InputDecoration(
-      hintText: 'Model',
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter model (Focus, Civic, etc)';
-      }
-      return _modelController.text;
-    },
-  );
-}
-
-Widget yearInput() {
-  return TextFormField(
-    controller: _yearController,
-    decoration: const InputDecoration(
-      hintText: 'Year',
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter year (2020, 1994, etc)';
-      }
-      return _yearController.text;
-    },
-  );
-}
-
-Widget odometerInput() {
-  return TextFormField(
-    controller: _odometerController,
-    decoration: const InputDecoration(
-      hintText: 'Odometer',
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter odometer';
-      }
-      return _odometerController.text;
-    },
-  );
-}
-
-Widget priceInput() {
-  return TextFormField(
-    controller: _priceController,
-    decoration: const InputDecoration(
-      hintText: 'Price',
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter price';
+      if (value == null || value.isEmpty) {
+        print(value);
+        return errorMessage;
       }
       return null;
     },
   );
 }
 
-Widget descriptionInput() {
-  return TextFormField(
-    controller: _descriptionController,
-    maxLength: null,
-    decoration: const InputDecoration(
-      hintText: 'Description',
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Enter price';
-      }
-      return _descriptionController.text;
-    },
-  );
-}
-
-Widget addCarButton() {
-  return ElevatedButton(onPressed: () => addCar(), child: Text('Add Car'));
-}
-
 Future addCar() async {
-  final _formKey = GlobalKey<FormState>();
-
-  final postListing = PostListing(
-      make: _makeController.text,
-      model: _modelController.text,
-      year: _yearController.text,
-      odometer: _odometerController.text,
-      price: _priceController.text,
-      description: _descriptionController.text,
-      dateAdded: DateTime.now().toString());
-  postListing.addPost();
+  Column(children: <Widget>[
+    inputField('Make', _makeController, 'Please enter a make'),
+    inputField('Model', _modelController, 'Please enter a model'),
+    inputField('Year', _yearController, 'Please enter a year'),
+    inputField('Odometer', _odometerController, 'Please enter an odometer'),
+    inputField('Price', _priceController, 'Please enter a price'),
+    inputField(
+        'Description', _descriptionController, 'Please enter a description'),
+  ]);
 }
+
 // _postingBloc.add(PostingEvent.postingSuccess());
+class PostConfirm extends StatefulWidget {
+  const PostConfirm({Key? key}) : super(key: key);
 
-Widget postConfirm() {
-  return Container(
-    // TODO change this height so its responsive
-    height: 500,
-    width: double.infinity,
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text(_yearController.text, style: const TextStyle(fontSize: 20)),
-              Text(
-                '${_makeController.text} ${_modelController.text}',
-                style: const TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Text(_priceController.text),
-          Text('${_odometerController.text} Miles'),
-          Text(_descriptionController.text),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text('Are you sure you want to post this car?'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              ElevatedButton(onPressed: null, child: Text('Yes')),
-              ElevatedButton(onPressed: null, child: Text('No')),
-            ],
-          ),
-        ],
-      ),
-    ),
-  );
+  @override
+  _PostConfirmState createState() => _PostConfirmState();
 }
+
+class _PostConfirmState extends State<PostConfirm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO change this height so its responsive
+      height: 500,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Placeholder(
+              fallbackHeight: 250,
+              fallbackWidth: 300,
+            ),
+            Row(
+              children: [
+                Text(
+                  _yearController.text,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                Text(
+                  ' ${_makeController.text}${_modelController.text}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+            Text(_priceController.text),
+            Text('${_odometerController.text} Miles'),
+            Text(_descriptionController.text),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Are you sure you want to post this car?'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () => addCar(), child: const Text('Yes')),
+                ElevatedButton(
+                    onPressed: Navigator.of(context).pop, child: Text('No')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// Widget postConfirm() {
+//   return Container(
+//     // TODO change this height so its responsive
+//     height: 500,
+//     width: double.infinity,
+//     child: Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           const Placeholder(
+//             fallbackHeight: 250,
+//             fallbackWidth: 300,
+//           ),
+//           Row(
+//             children: [
+//               Text(
+//                 _yearController.text,
+//                 style: const TextStyle(fontSize: 20),
+//               ),
+//               Text(
+//                 ' ${_makeController.text}${_modelController.text}',
+//                 style: const TextStyle(fontSize: 20),
+//               ),
+//             ],
+//           ),
+//           Text(_priceController.text),
+//           Text('${_odometerController.text} Miles'),
+//           Text(_descriptionController.text),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           const Text('Are you sure you want to post this car?'),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               ElevatedButton(
+//                   onPressed: () => addCar(), child: const Text('Yes')),
+//               ElevatedButton(onPressed: , child: Text('No')),
+//             ],
+//           ),
+//         ],
+//       ),
+//     ),
+//   );
+// }
