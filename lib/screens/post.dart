6d01f1ea362f5b20971fab59719dccdd5c5d6867
dom thomas <domtom1126@gmail.com>
2022-02-16 11:35:12 -0500

    Image picker button works for gallery

--- lib/screens/post.dart
+++ lib/screens/post.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:testing/listing_bloc.dart';
 import 'package:testing/widgets/form_widgets/form_widgets.dart';
 
@@ -11,7 +14,14 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
+  final TextEditingController _makeController = TextEditingController();
+  final TextEditingController _modelController = TextEditingController();
+  final TextEditingController _yearController = TextEditingController();
+  final TextEditingController _odometerController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  final ImagePicker _imagePicker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,26 +39,32 @@ class _PostState extends State<Post> {
             //       color: Colors.grey,
             //     ),
             //     borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Column(children: [
-              makeInput(),
-              const SizedBox(height: 20),
-              modelInput(),
-              const SizedBox(height: 20),
-              yearInput(),
-              const SizedBox(height: 20),
-              odometerInput(),
-              const SizedBox(height: 20),
-              priceInput(),
-              const SizedBox(height: 20),
-              descriptionInput(),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                  onPressed: () => showBottomSheet(
-                        context: context,
-                        builder: (context) => postConfirm(),
-                      ),
-                  child: const Text('Confirm')),
-            ]),
+            child: Column(
+              children: [
+                inputField('Make', _makeController, 'Enter make'),
+                const SizedBox(height: 20),
+                inputField('Model', _modelController, 'Enter model'),
+                const SizedBox(height: 20),
+                inputField('Year', _yearController, 'Enter year'),
+                const SizedBox(height: 20),
+                inputField('Odometer', _odometerController, 'Enter odometer'),
+                const SizedBox(height: 20),
+                inputField('Price', _priceController, 'Enter price'),
+                const SizedBox(height: 20),
+                inputField(
+                    'Description', _descriptionController, 'Enter description'),
+                ElevatedButton(
+                    onPressed: () => pickImage(),
+                    child: const Text('Add Image')),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () => showBottomSheet(
+                          context: context,
+                          builder: (context) => const PostConfirm(),
+                        ),
+                    child: const Text('Confirm')),
+              ],
+            ),
           ),
         ),
       ),
