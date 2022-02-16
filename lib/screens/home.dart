8d3cc02c1203a84818c392ced230747d2229b93d
dom thomas <domtom1126@gmail.com>
2022-02-15 23:32:41 -0500

    added input widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:testing/firebase_functions/get_listings.dart';
+import 'package:testing/screens/view_car.dart';
 
 import '../listing.dart';
-import '../listing_cubit.dart';
-import 'listings_view.dart';
+import '../listing_bloc.dart';
 
-// todo this page will get the most recent listings. Wrap with BlocBuilder
+// TODO this page will get the most recent listings. Wrap with BlocBuilder
 class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  final listingView = ListingView();
-
+  final cars = FirebaseFirestore.instance
+      .collection('posts')
+      .orderBy('date_added', descending: true)
+      .snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: listingView.getListings(),
+      body: StreamBuilder(
+        stream: cars,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return ListView(
+                children: snapshot.data!.docs.map((publicList) {
+              return ListTile(
+                // TODO maybe add some "leading:" text?
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Placeholder(
+                      fallbackHeight: 250,
+                    ),
+                    Text(
+                      '${publicList['price']}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      '${publicList['year']} ${publicList['make']} ${publicList['model']}',
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                subtitle: Text('${publicList['odometer']} Miles'),
+                onTap: () {
+                  showBottomSheet(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    context: context,
+                    builder: (context) => ViewCar(
+                      publicList['make'],
+                      publicList['model'],
+                      publicList['year'],
+                      publicList['price'],
+                      publicList['odometer'],
+                      publicList['description'],
+                    ),
+                  );
+                },
+              );
+            }).toList());
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
