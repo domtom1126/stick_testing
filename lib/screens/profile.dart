f86bd0a71e0c6c4ed73be84cac68b38cc37da012
dom thomas <domtom1126@gmail.com>
2022-03-04 22:25:15 -0500

    Wait that was wrong! This has log in/log out buttons

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:testing/screens/create_account.dart';
+import 'package:testing/screens/sign_in.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -8,14 +11,50 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: const Center(
-        child: Text('Profile'),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignInPage()));
+                },
+                child: const Text('Sign In')),
+            if (auth.currentUser != null)
+              ElevatedButton(
+                  onPressed: () {
+                    auth.signOut();
+                  },
+                  child: const Text('Sign Out')),
+            if (auth.currentUser == null)
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CreateAccount()));
+                  },
+                  child: const Text('Create an Account')),
+            if (auth.currentUser != null)
+              Container(
+                child: Text('Signed in'),
+              ),
+            if (auth.currentUser == null)
+              Container(
+                child: Text('Signed out'),
+              ),
+          ],
+        ),
       ),
     );
   }
