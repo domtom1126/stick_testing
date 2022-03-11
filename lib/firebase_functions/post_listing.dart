56807236287cb0fa1f9382adf3fb2158e1cb80fc
dom thomas <domtom1126@gmail.com>
2022-03-11 11:50:41 -0500

    image_picker not working removed params

--- lib/firebase_functions/post_listing.dart
+++ lib/firebase_functions/post_listing.dart
@@ -8,47 +8,45 @@ import 'package:testing/listing.dart';
 // * This would be like data_service.dart
 
 class PostListing {
-  String id = '';
   String make = '';
   String model = '';
   String year = '';
   String odometer = '';
   String price = '';
   String description = '';
-  String image = '';
+  // String image = '';
   String dateAdded = '';
 
   PostListing({
-    // required this.id,
     required this.make,
     required this.model,
     required this.year,
     required this.odometer,
     required this.price,
     required this.description,
-    required this.image,
+    // required this.image,
     required this.dateAdded,
   });
 
   addPost(String make, String model, String year, String odometer, String price,
-      String description, String image) async {
+      String description) async {
     // TODO Images are gonna upload here
-    final snapshot = firebase_storage.FirebaseStorage.instance
-        .ref('images/')
-        .putFile(File(image))
-        .snapshot;
-    final imageURL = await snapshot.ref.getDownloadURL();
+    // final snapshot = firebase_storage.FirebaseStorage.instance
+    //     .ref()
+    //     .child('images/')
+    //     .putFile(File(image.path))
+    //     .snapshot;
+    // final imageURL = await snapshot.ref.getDownloadURL();
     CollectionReference addPost =
         FirebaseFirestore.instance.collection('posts');
     await addPost.add({
-      'id': id,
       'make': make,
       'model': model,
       'year': year,
       'odometer': odometer,
       'price': price,
       'description': description,
-      'image': imageURL,
+      // 'image': imageURL,
       'date_added': DateTime.now(),
     });
   }
