3d59213db563ad4091132b414f361ead583ca5b2
dom thomas <domtom1126@gmail.com>
2022-03-21 15:44:14 -0400

    Added read more button for descr.

--- lib/screens/view_car.dart
+++ lib/screens/view_car.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:readmore/readmore.dart';
 
 class ViewCar extends StatefulWidget {
   final String make;
@@ -23,82 +25,112 @@ class _ViewCarState extends State<ViewCar> {
     return carModal();
   }
 
-  SizedBox carModal() {
-    return SizedBox(
-      height: 500,
-      child: Column(
-        // TODO change this height so its responsive
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(widget.image),
-                Row(
-                  children: [
-                    Text('${widget.year} ',
-                        style: const TextStyle(fontSize: 20)),
-                    Text(
-                      '${widget.make} ${widget.model}',
-                      style: const TextStyle(fontSize: 20),
+  ListView carModal() {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                constraints: BoxConstraints(maxWidth: 400, maxHeight: 200),
+                child: FittedBox(
+                  child: Image.network(widget.image),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Row(
+                children: [
+                  Text('${widget.year} ',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: HexColor('FFFFFF'),
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    '${widget.make} ${widget.model}',
+                    style: TextStyle(fontSize: 20, color: HexColor('FFFFFF')),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                '${widget.odometer} Miles',
+                style: TextStyle(color: HexColor('FFFFFF'), fontSize: 15),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                widget.price,
+                style: TextStyle(color: HexColor('FFFFFF'), fontSize: 15),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ReadMoreText(
+                '${widget.description} ',
+                moreStyle: TextStyle(
+                    color: HexColor('FFFFFF'),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+                trimLines: 2,
+                trimMode: TrimMode.Line,
+                trimCollapsedText: 'Read More',
+                trimExpandedText: 'Read Less',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: onLiked ? Colors.red : Colors.grey,
                     ),
-                  ],
-                ),
-                Text(widget.price),
-                Text('${widget.odometer} Miles'),
-                Text(widget.description),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: onLiked ? Colors.red : Colors.grey,
-                      ),
-                      child: const Icon(Icons.favorite),
-                      onPressed: () {
-                        // get current user id
-                        // add to favorites
-                        // CollectionReference addToLiked =
-                        //     FirebaseFirestore.instance.collection('${c} liked');
-                        setState(() {
-                          onLiked = !onLiked;
-                        });
-                        final snackBar = SnackBar(
-                          duration: const Duration(seconds: 2),
-                          content: const Text('Car Liked'),
-                          action: SnackBarAction(
-                            label: 'Undo',
-                            onPressed: () {
-                              // Some code to undo the change.
-                            },
-                          ),
-                        );
+                    child: const Icon(Icons.favorite),
+                    onPressed: () {
+                      // get current user id
+                      // add to favorites
+                      // CollectionReference addToLiked =
+                      //     FirebaseFirestore.instance.collection('${c} liked');
+                      setState(() {
+                        onLiked = !onLiked;
+                      });
+                      final snackBar = SnackBar(
+                        duration: const Duration(seconds: 2),
+                        content: const Text('Car Liked'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          onPressed: () {
+                            // Some code to undo the change.
+                          },
+                        ),
+                      );
 
-                        // Find the ScaffoldMessenger in the widget tree
-                        // and use it to show a SnackBar.
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      },
-                    ),
-                    ElevatedButton(
-                        onPressed: () => showDialog(
-                              context: context,
-                              builder: (context) => const AlertDialog(
-                                title: Text('Email the Owner'),
-                                content: Text('Email the Owner'),
-                              ),
+                      // Find the ScaffoldMessenger in the widget tree
+                      // and use it to show a SnackBar.
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                  ),
+                  ElevatedButton(
+                      onPressed: () => showDialog(
+                            context: context,
+                            builder: (context) => const AlertDialog(
+                              title: Text('Email the Owner'),
+                              content: Text('Email the Owner'),
                             ),
-                        child: const Icon(Icons.email)),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+                          ),
+                      child: const Icon(Icons.email)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
