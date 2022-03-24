a0c074126c8ce21a36fbed9ebd237ff47b2cbb3f
dom thomas <domtom1126@gmail.com>
2022-03-23 21:53:35 -0400

    Got rid of getx

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,33 +1,108 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:find_a_stick/signin_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
-import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hexcolor/hexcolor.dart';
-
-import '../signin_controller.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
-  _ProfileState createState() => _ProfileState();
+  State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
-  final controller = Get.put(LoginController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: Text('Profile'),
       ),
-      body: Obx(() {
-        if (controller.googleAccount.value == null) {
-          return buildSignIn(context);
-        } else {
-          return buildProfilePage(context);
-        }
-      }),
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              return buildProfilePage(context);
+            } else {
+              return buildSignIn(context);
+            }
+          }),
+    );
+  }
+
+  Center buildProfilePage(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    return Center(
+      child: Container(
+        height: 450,
+        width: 300,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.grey[800],
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(children: [
+          const SizedBox(
+            height: 20,
+          ),
+          CircleAvatar(
+            backgroundImage: NetworkImage(user.photoURL!),
+            radius: 50,
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(user.displayName!,
+              style: TextStyle(fontSize: 20, color: HexColor('FFFFFF'))),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(user.email!,
+              style: TextStyle(fontSize: 20, color: HexColor('FFFFFF'))),
+          const SizedBox(
+            height: 30,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                onPressed: () {
+                  null;
+                },
+                child: const Text('Edit Profile')),
+          ),
+          // Align(
+          //   alignment: Alignment.bottomCenter,
+          //   child: ElevatedButton(
+          //       onPressed: () => showModalBottomSheet(
+          //           backgroundColor: HexColor('40434E'),
+          //           shape: const RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.only(
+          //               topLeft: Radius.circular(20),
+          //               topRight: Radius.circular(20),
+          //             ),
+          //           ),
+          //           context: context,
+          //           builder: showUserCars),
+          //       child: const Text('Your Cars')),
+          // ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                onPressed: () {
+                  null;
+                },
+                child: const Text('Sign Out')),
+          ),
+        ]),
+      ),
     );
   }
 
@@ -67,8 +142,10 @@ class _ProfileState extends State<Profile> {
             SizedBox(
               width: 200,
               child: SignInButton(Buttons.Google, onPressed: () {
-                controller.googleLogin();
-              }, text: 'Sign in with Google'),
+                final provider =
+                    Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.googleLogin();
+              }),
             ),
             SizedBox(
               width: 200,
@@ -80,122 +157,6 @@ class _ProfileState extends State<Profile> {
           ],
         ),
       ),
-    );
-  }
-
-  Center buildProfilePage(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 450,
-        width: 300,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.grey[800],
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(children: [
-          const SizedBox(
-            height: 20,
-          ),
-          CircleAvatar(
-            backgroundImage:
-                NetworkImage(controller.googleAccount.value?.photoUrl ?? ''),
-            radius: 50,
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Text(controller.googleAccount.value?.displayName ?? '',
-              style: TextStyle(fontSize: 20, color: HexColor('FFFFFF'))),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(controller.googleAccount.value?.email ?? '',
-              style: TextStyle(fontSize: 20, color: HexColor('FFFFFF'))),
-          const SizedBox(
-            height: 30,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-                onPressed: () {
-                  null;
-                },
-                child: const Text('Edit Profile')),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-                onPressed: () => showModalBottomSheet(
-                    backgroundColor: HexColor('40434E'),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                    ),
-                    context: context,
-                    builder: showUserCars),
-                child: const Text('Your Cars')),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-                onPressed: () {
-                  controller.googleLogout();
-                },
-                child: const Text('Sign Out')),
-          ),
-        ]),
-      ),
-    );
-  }
-
-  ListView showUserCars(BuildContext context) {
-    final userCars = FirebaseFirestore.instance
-        .collection('posts')
-        .where('email', isEqualTo: controller.googleAccount.value!.email)
-        .snapshots();
-    return ListView(
-      children: [
-        Column(children: [
-          const SizedBox(
-            height: 20,
-          ),
-          StreamBuilder(
-            stream: userCars,
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  children: snapshot.data!.docs.map((userCars) {
-                    return ListTile(
-                      title: Text(
-                          '${userCars['year']} ${userCars['make']} ${userCars['model']}',
-                          style: TextStyle(
-                              fontSize: 16, color: HexColor('FFFFFF'))),
-                      subtitle: TextButton(
-                          onPressed: null, child: Text('Mark as Sold')),
-                      // Delete post button
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          FirebaseFirestore.instance
-                              .collection('posts')
-                              .doc(userCars.id)
-                              .delete();
-                        },
-                      ),
-                    );
-                  }).toList(),
-                );
-              } else {
-                return const Text('You haven\'t posted any cars yet!');
-              }
-            },
-          )
-        ])
-      ],
     );
   }
 }
