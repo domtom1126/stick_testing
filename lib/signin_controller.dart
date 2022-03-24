22b24dad157631832acb04ff3296d7bb57e09bcc
dom thomas <domtom1126@gmail.com>
2022-03-23 21:53:57 -0400

    Got rid of getx. Lol. Using provider for now

--- lib/signin_controller.dart
+++ lib/signin_controller.dart
@@ -1,16 +1,26 @@
-import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginController extends GetxController {
-  var _googleSignIn = GoogleSignIn();
-  var googleAccount = Rx<GoogleSignInAccount?>(null);
+class GoogleSignInProvider extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+  // * This contains all stuff about current user. Name, etc.
+  GoogleSignInAccount? _currentUser;
+  GoogleSignInAccount? get currentUser => _currentUser!;
 
-  login() async {
-    googleAccount.value = await _googleSignIn.signIn();
-  }
+  Future googleLogin() async {
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser == null) return;
+    _currentUser = googleUser;
 
-  logout() async {
-    await _googleSignIn.signOut();
-    googleAccount.value = null;
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await FirebaseAuth.instance.signInWithCredential(
+      credential,
+    );
+    notifyListeners();
   }
 }
