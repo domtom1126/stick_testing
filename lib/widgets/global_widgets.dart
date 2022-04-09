f2894c812ce6dc6b8c9f6fc96546784bbfe0d35b
dom thomas <domtom1126@gmail.com>
2022-04-08 20:35:52 -0400

    Removed bottom line

--- lib/widgets/global_widgets.dart
+++ lib/widgets/global_widgets.dart
@@ -3,13 +3,6 @@ import 'package:hexcolor/hexcolor.dart';
 
 AppBar appBar(BuildContext context, String title) {
   return AppBar(
-    bottom: PreferredSize(
-      child: Container(
-        color: HexColor('EE6C4D'),
-        height: 1,
-      ),
-      preferredSize: const Size.fromHeight(1),
-    ),
     backgroundColor: Colors.transparent,
     elevation: 0,
     centerTitle: false,
