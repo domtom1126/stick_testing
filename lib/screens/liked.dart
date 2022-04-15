2538ce2b8c647282b175b9a8f68a1c5db2a633df
dom thomas <domtom1126@gmail.com>
2022-04-14 23:17:16 -0400

    Sliver AppBar Works

--- lib/screens/liked.dart
+++ lib/screens/liked.dart
@@ -20,20 +20,35 @@ class _LikedState extends State<Liked> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBar(context, 'Liked'),
-        body: StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasData) {
-                return userLikedCars();
-              } else {
-                return buildSignIn(context);
-              }
-            }));
+        body: NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverAppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          backgroundColor: Colors.transparent,
+          floating: true,
+          pinned: false,
+          snap: false,
+          title: Text(
+            'Liked Cars',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ],
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              return userLikedCars();
+            } else {
+              return buildSignIn(context);
+            }
+          }),
+    ));
   }
 
   StreamBuilder<QuerySnapshot<Map<String, dynamic>>> userLikedCars() {
@@ -46,77 +61,80 @@ class _LikedState extends State<Liked> {
       stream: cars,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasData) {
-          return ListView(
-              children: snapshot.data!.docs.map((userLikedCars) {
-            return ListTile(
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet(
-                        isScrollControlled: true,
-                        backgroundColor: HexColor('40434E'),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20),
-                          ),
-                        ),
-                        context: context,
-                        builder: (context) => ViewCar(
-                          userLikedCars['make'],
-                          userLikedCars['model'],
-                          userLikedCars['year'],
-                          userLikedCars['price'],
-                          userLikedCars['odometer'],
-                          userLikedCars['image'],
-                          userLikedCars['description'],
-                          userLikedCars['email'],
-                          userLikedCars.id,
-                        ),
-                      );
-                    },
-                    child: SizedBox(
+          return CustomScrollView(
+              slivers: snapshot.data!.docs.map((publicList) {
+            return SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
                       height: 200,
                       width: double.infinity,
                       child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: CachedNetworkImage(
-                          imageUrl: userLikedCars['image'],
-                          fit: BoxFit.fitWidth,
-                          placeholder: (context, url) =>
-                              const Center(child: CircularProgressIndicator()),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
-                        ),
-                      ),
+                          borderRadius: BorderRadius.circular(10),
+                          child: GestureDetector(
+                            onTap: () => {
+                              showModal(context, publicList),
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl: publicList['image'],
+                              fit: BoxFit.fitWidth,
+                              placeholder: (context, url) => const Center(
+                                  child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          )),
                     ),
-                  ),
-                  // const SizedBox(
-                  //   height: 10,
-                  // ),
+                    // const SizedBox(
+                    //   height: 10,
+                    // ),
 
-                  Text(
-                    '${userLikedCars['year']} ${userLikedCars['make']} ${userLikedCars['model']}',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  Text(
-                    '${userLikedCars['price']}',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    textAlign: TextAlign.start,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                ],
-              ),
-              subtitle: Text(
-                '${userLikedCars['odometer']} Miles',
-                style: Theme.of(context).textTheme.bodySmall,
+                    Row(
+                      children: [
+                        Text(
+                          '${publicList['year']} ${publicList['make']} ${publicList['model']}',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                        if (FirebaseAuth.instance.currentUser?.uid == null)
+                          Container()
+                        // TODO add like button to let user know if theyve liked current car
+                        // else
+                        //   IconButton(
+                        //     icon: Icon(Icons.favorite, color: Colors.red),
+                        //     onPressed: () {
+                        //       // FirebaseFirestore.instance
+                        //       //     .collection('posts')
+                        //       //     .doc(widget.docId)
+                        //       //     .update({
+                        //       //   'likedIds': FieldValue.arrayRemove([uid])
+                        //       // });
+                        //       // setState(() {
+                        //       //   onLiked = false;
+                        //       // });
+                        //     },
+                        //   )
+                      ],
+                    ),
+                    Text(
+                      '${publicList['price']}',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.start,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '${publicList['odometer']} Miles',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
               ),
             );
           }).toList());
@@ -126,6 +144,32 @@ class _LikedState extends State<Liked> {
           );
         }
       },
+    );
+  }
+
+  Future<dynamic> showModal(
+      BuildContext context, QueryDocumentSnapshot<Object?> userLikedCars) {
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      backgroundColor: HexColor('40434E'),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      context: context,
+      builder: (context) => ViewCar(
+        userLikedCars['make'],
+        userLikedCars['model'],
+        userLikedCars['year'],
+        userLikedCars['price'],
+        userLikedCars['odometer'],
+        userLikedCars['image'],
+        userLikedCars['description'],
+        userLikedCars['email'],
+        userLikedCars.id,
+      ),
     );
   }
 
