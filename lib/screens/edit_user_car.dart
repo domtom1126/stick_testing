f306a494bbd68e06e1368aa79ce149db9252fc27
dom thomas <domtom1126@gmail.com>
2022-04-21 15:25:39 -0400

    Updating works

--- lib/screens/edit_user_car.dart
+++ lib/screens/edit_user_car.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditUserCar extends StatefulWidget {
   final String make;
@@ -9,6 +15,7 @@ class EditUserCar extends StatefulWidget {
   final String odometer;
   final String image;
   final String description;
+  final String id;
   const EditUserCar(
       {Key? key,
       required this.make,
@@ -17,7 +24,8 @@ class EditUserCar extends StatefulWidget {
       required this.price,
       required this.odometer,
       required this.image,
-      required this.description})
+      required this.description,
+      required this.id})
       : super(key: key);
 
   @override
@@ -25,81 +33,235 @@ class EditUserCar extends StatefulWidget {
 }
 
 class _EditUserCarState extends State<EditUserCar> {
+  final _formKey = GlobalKey<FormState>();
+
+  File? pickedImage;
+  Future pickImage() async {
+    final _imagePicker = ImagePicker();
+    final pickedImage =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    final imageTemp = File(pickedImage!.path);
+    setState(() => this.pickedImage = imageTemp);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _makeController =
+        TextEditingController(text: widget.make);
+    final TextEditingController _modelController =
+        TextEditingController(text: widget.model);
+    final TextEditingController _yearController =
+        TextEditingController(text: widget.year);
+    final TextEditingController _odometerController =
+        TextEditingController(text: widget.odometer);
+    final TextEditingController _priceController =
+        TextEditingController(text: widget.price);
+    final TextEditingController _descriptionController =
+        TextEditingController(text: widget.description);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit User Car'),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.make,
-            decoration: const InputDecoration(
-              labelText: 'Make',
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.model,
-            decoration: const InputDecoration(
-              labelText: 'Model',
+            TextFormField(
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(20),
+              ],
+              controller: _makeController,
+              keyboardAppearance: Brightness.dark,
+              decoration: InputDecoration(
+                labelText: 'Make',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Make',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a make';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.year,
-            decoration: const InputDecoration(
-              labelText: 'Year',
+            const SizedBox(height: 20),
+            TextFormField(
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(20),
+              ],
+              controller: _modelController,
+              keyboardAppearance: Brightness.dark,
+              decoration: InputDecoration(
+                labelText: 'Model',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Model',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a make';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.price,
-            decoration: const InputDecoration(
-              labelText: 'Price',
+            const SizedBox(height: 20),
+            TextFormField(
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(4),
+              ],
+              keyboardType: TextInputType.number,
+              keyboardAppearance: Brightness.dark,
+              controller: _yearController,
+              decoration: InputDecoration(
+                labelText: 'Year',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Year',
+              ),
+              validator: (value) {
+                if (value!.length != 4) {
+                  return 'Please enter a valid year';
+                } else if (value[0] == '0' ||
+                    value[0] == '3' ||
+                    value[0] == '4' ||
+                    value[0] == '5' ||
+                    value[0] == '6' ||
+                    value[0] == '7' ||
+                    value[0] == '8' ||
+                    value[0] == '9') {
+                  return 'Enter 2 digit valid year';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.odometer,
-            decoration: const InputDecoration(
-              labelText: 'Odometer',
+            const SizedBox(height: 20),
+            TextFormField(
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(10),
+              ],
+              controller: _odometerController,
+              keyboardAppearance: Brightness.dark,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Odometer',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Miles',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a miles';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            initialValue: widget.description,
-            decoration: const InputDecoration(
-              labelText: 'Description',
+            const SizedBox(height: 20),
+            TextFormField(
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(10),
+                CurrencyTextInputFormatter(symbol: '\$', decimalDigits: 0),
+              ],
+              controller: _priceController,
+              keyboardAppearance: Brightness.dark,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Price',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Price',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a price';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            child: const Text('Save'),
-            onPressed: () {
-              null;
-            },
-          ),
-        ],
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _descriptionController,
+              keyboardAppearance: Brightness.dark,
+              decoration: InputDecoration(
+                labelText: 'Description',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: HexColor('EE815A'), width: 2.0),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                hintText: 'Description',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a description';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            // Show Image
+            // pickedImage != null
+            // Image.network(
+            //   widget.image,
+            //   fit: BoxFit.cover,
+            // ),
+            Container(),
+
+            // * This actually works! but for now just do single image
+            // CarouselSlider.builder(
+            //   options: CarouselOptions(height: 200),
+            //   itemCount: pickedImage.length,
+            //   itemBuilder: (context, index, realIndex) {
+            //     final singleImage = pickedImage[index];
+
+            //     return Container(
+            //       margin: EdgeInsets.all(5.0),
+            //       child: ClipRRect(
+            //         borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            //         child: Image.file(
+            //           singleImage!,
+            //           fit: BoxFit.cover,
+            //         ),
+            //       ),
+            //     );
+            //   },
+            // ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // save data to post
+                    FirebaseFirestore.instance
+                        .collection('posts')
+                        .doc(widget.id)
+                        .update({
+                      'make': _makeController.text,
+                      'model': _modelController.text,
+                      'year': _yearController.text,
+                      'odometer': _odometerController.text,
+                      'price': _priceController.text,
+                      'description': _descriptionController.text,
+                      'image': widget.image,
+                    }).whenComplete(() => Navigator.pop(context));
+                  }
+                },
+                child: const Text('Save')),
+          ],
+        ),
       ),
     );
   }
