b7410782153065dd507762d96e1bd5744c95951c
dom thomas <domtom1126@gmail.com>
2022-06-01 19:14:14 -0400

    Moved sign in widget to seperate file

---
+++ lib/screens/profile_widgets/sign_in.dart
@@ -0,0 +1,80 @@
+import 'package:find_a_stick/signin_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:provider/provider.dart';
+
+class ProfileSignIn extends StatefulWidget {
+  const ProfileSignIn({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileSignIn> createState() => _ProfileSignInState();
+}
+
+class _ProfileSignInState extends State<ProfileSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Widget buildSignIn(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[800],
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        height: 400,
+        width: 300,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 25,
+            ),
+            Text(
+              'You\'re not logged in!',
+              style: TextStyle(color: HexColor('FFFFFF'), fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Sign in below and you will see your profile after you sign in.',
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const Divider(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SignInButton(Buttons.Google,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ), onPressed: () {
+                final provider =
+                    Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.googleLogin();
+              }),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SignInButton(Buttons.Apple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ), onPressed: () {
+                final appleProvider =
+                    Provider.of<AppleSignInProvider>(context, listen: false);
+                appleProvider.appleLogin();
+              }, text: 'Sign in with Apple'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
