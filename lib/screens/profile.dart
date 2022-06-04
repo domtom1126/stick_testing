5f941eb048a5ed751070519bd86cef5491eae36e
dom thomas <domtom1126@gmail.com>
2022-06-03 21:15:45 -0400

    Moved some files around

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:find_a_stick/screens/edit_user_car.dart';
+import 'package:find_a_stick/screens/profile_widgets/profile_screen.dart';
+import 'package:find_a_stick/screens/profile_widgets/sign_in.dart';
 import 'package:find_a_stick/signin_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
 
@@ -17,10 +15,7 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   bool isSold = false;
-  final userCars = FirebaseFirestore.instance
-      .collection('posts')
-      .where('id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-      .snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,254 +42,11 @@ class _ProfileState extends State<Profile> {
                 child: CircularProgressIndicator(),
               );
             } else if (snapshot.hasData) {
-              return buildProfilePage(context);
+              return const ProfilePage();
             } else {
-              return buildSignIn(context);
+              return const ProfileSignIn();
             }
           }),
-    );
-  }
-
-  Widget buildProfilePage(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(user.photoURL ?? ''),
-              radius: 60,
-            ),
-            // const SizedBox(
-            //   width: 50,
-            // ),
-            Column(
-              children: [
-                // TODO make edit button work
-                // editButton(),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(user.displayName ?? '',
-                    style: TextStyle(fontSize: 18, color: HexColor('FFFFFF'))),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(user.email ?? 'No email found',
-                    style: TextStyle(fontSize: 18, color: HexColor('FFFFFF'))),
-                const SizedBox(
-                  height: 30,
-                ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Center(
-          child:
-              Text('Your cars', style: Theme.of(context).textTheme.headline6),
-        ),
-        const Center(child: Text('Click to edit details')),
-        const SizedBox(
-          height: 20,
-        ),
-        ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.4,
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-            color: HexColor('23262F'),
-            child: StreamBuilder(
-              stream: userCars,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: Text(
-                      'You have not posted any cars yet.',
-                    ),
-                  );
-                } else if (snapshot.hasData) {
-                  return ListView(
-                    shrinkWrap: true,
-                    children: snapshot.data!.docs.map((userCars) {
-                      return ListTile(
-                        onTap: () {
-                          // Navigator.pop(context);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EditUserCar(
-                                make: userCars['make'],
-                                model: userCars['model'],
-                                year: userCars['year'],
-                                price: userCars['price'],
-                                odometer: userCars['odometer'],
-                                image: userCars['image'],
-                                description: userCars['description'],
-                                id: userCars.id,
-                              ),
-                            ),
-                          );
-                        },
-                        title: Text(
-                          '${userCars['year']} ${userCars['make']} ${userCars['model']}',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        subtitle: (isSold)
-                            ? TextButton(
-                                // TODO add mark as sold button
-                                onPressed: () {
-                                  FirebaseFirestore.instance
-                                      .collection('posts')
-                                      .doc(userCars.id)
-                                      .update({
-                                    'sold': false,
-                                  });
-                                  setState(() {
-                                    isSold = false;
-                                  });
-                                },
-                                child: Text(
-                                  'Mark as not sold',
-                                  style: TextStyle(color: HexColor('FFFFFF')),
-                                  textAlign: TextAlign.left,
-                                ))
-                            : TextButton(
-                                // TODO add mark as sold button
-                                onPressed: () {
-                                  FirebaseFirestore.instance
-                                      .collection('posts')
-                                      .doc(userCars.id)
-                                      .update({
-                                    'sold': true,
-                                  });
-                                  setState(() {
-                                    isSold = true;
-                                  });
-                                },
-                                child: Text(
-                                  'Mark as Sold',
-                                  style: TextStyle(color: HexColor('FFFFFF')),
-                                  textAlign: TextAlign.left,
-                                )),
-                        // Delete post button
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.white),
-                          onPressed: () async {
-                            await HapticFeedback.heavyImpact();
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    backgroundColor: Colors.black45,
-                                    title: const Text('Are you sure?'),
-                                    content: Text(
-                                        'Do you want to delete this car?',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium),
-                                    actions: [
-                                      ElevatedButton(
-                                        child: const Text('Yes'),
-                                        onPressed: () {
-                                          FirebaseFirestore.instance
-                                              .collection('posts')
-                                              .doc(userCars.id)
-                                              .delete();
-                                          Navigator.of(context).pop();
-                                        },
-                                      ),
-                                      ElevatedButton(
-                                        child: const Text('No'),
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                      )
-                                    ],
-                                  );
-                                });
-                          },
-                        ),
-                      );
-                    }).toList(),
-                  );
-                } else {
-                  return const Text('Loading...');
-                }
-              },
-            ),
-          ),
-        ),
-      ]),
-    );
-  }
-
-  Center buildSignIn(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[800],
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        height: 400,
-        width: 300,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 25,
-            ),
-            Text(
-              'You\'re not logged in!',
-              style: TextStyle(color: HexColor('FFFFFF'), fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Sign in below and you will see your profile after you sign in.',
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Divider(
-              height: 50,
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SignInButton(Buttons.Google,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ), onPressed: () {
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                provider.googleLogin();
-              }),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SignInButton(Buttons.Apple,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ), onPressed: () {
-                final appleProvider =
-                    Provider.of<AppleSignInProvider>(context, listen: false);
-                appleProvider.appleLogin();
-              }, text: 'Sign in with Apple'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
