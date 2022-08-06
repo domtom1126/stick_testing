0ba30db37c551cacff5730ba7938cfc33e99a110
dom thomas <domtom1126@gmail.com>
2022-08-05 23:19:51 -0400

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
