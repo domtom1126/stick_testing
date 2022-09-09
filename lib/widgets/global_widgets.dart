63a53ef112e02637dae864c25e5c38884f9419c8
dom thomas <domtom1126@gmail.com>
2022-09-08 23:49:43 -0400

    Added signin/out function

--- lib/widgets/global_widgets.dart
+++ lib/widgets/global_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 AppBar appBar(BuildContext context, String title) {
@@ -19,4 +20,32 @@ extension StringCasingExtension on String {
       .split(' ')
       .map((str) => str.toCapitalized())
       .join(' ');
+}
+
+// TODO Change these print calls
+
+signInUp(String email, String password) async {
+  try {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      try {
+        FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'wrong-password') {
+          print('wrong password');
+        }
+      }
+    }
+  } catch (e) {
+    print(e);
+  }
 }
