93aa29a1721450c95312e2ef4cc715984bb38c2e
dom thomas <domtom1126@gmail.com>
2022-10-21 20:03:48 -0400

    Email/Pass works. Fixed a keyboard renderflow issue.

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -28,170 +28,208 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
   final navigatorKey = GlobalKey<NavigatorState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool loginFail = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Center(
-      child: CustomPaint(
-        painter: OrangeLines(),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 200,
-            ),
-            Text(
-              'Find a Stick',
-              style: TextStyle(color: HexColor('FFFFFF'), fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-              child: Text(
-                'The only app to find manual transmission vehicles',
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: _emailController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  hintText: 'Email',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: _passwordController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  hintText: 'Password',
-                ),
-                validator: (value) {
-                  // TODO put proper validation for password
-                  if (value!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 250,
-              child: ElevatedButton(
-                // TODO do something after they click sign in sign up
-                onPressed: () async {
-                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                  );
-                },
-                child: const Text('Sign in / up'),
-              ),
-            ),
-            SizedBox(
-              width: 250,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                ),
-                onPressed: () async {
-                  final prefs = await SharedPreferences.getInstance();
-                  prefs.setBool('showHome', true);
-
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const BottomBar()));
-                },
-                child: const Text('Skip this step'),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: CustomPaint(
+            painter: OrangeLines(),
+            child: Column(
               children: [
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                      ),
-                      child: const Image(
-                          image: AssetImage('graphics/icons8-google-48.png')),
-                      onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
-                            context,
-                            listen: false);
-                        provider.googleLogin();
-                      }),
+                const SizedBox(
+                  height: 200,
+                ),
+                Text(
+                  'Find a Stick',
+                  style: TextStyle(color: HexColor('FFFFFF'), fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: Text(
+                    'The only app to find manual transmission vehicles',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
                 ),
                 SizedBox(
-                  width: 100,
+                  width: 300,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(20),
+                    ],
+                    controller: _emailController,
+                    keyboardAppearance: Brightness.dark,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: HexColor('EE815A'), width: 2.0),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      hintText: 'Email',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter an email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.next,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(20),
+                    ],
+                    controller: _passwordController,
+                    keyboardAppearance: Brightness.dark,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: HexColor('EE815A'), width: 2.0),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      hintText: 'Password',
+                    ),
+                    validator: (value) {
+                      // TODO put proper validation for password
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 250,
+                  child: ElevatedButton(
+                    // TODO validation for email and password
+                    onPressed: () async {
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        );
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const BottomBar()),
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'email-already-in-use') {
+                          await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: _emailController.text,
+                                  password: _passwordController.text);
+                          // TODO Add validation
+                          if (e.code == 'wrong-password') {}
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const BottomBar()));
+                        }
+                      }
+                    },
+                    child: const Text('Sign in / up'),
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.transparent,
                     ),
-                    onPressed: () {
-                      final appleProvider = Provider.of<AppleSignInProvider>(
+                    onPressed: () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      prefs.setBool('showHome', true);
+
+                      Navigator.pushReplacement(
                           context,
-                          listen: false);
-                      appleProvider.appleLogin();
+                          MaterialPageRoute(
+                              builder: (context) => const BottomBar()));
                     },
-                    child: const Image(
-                      image: AssetImage('graphics/icons8-apple-logo-50.png'),
-                    ),
+                    child: const Text('Skip this step'),
                   ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                          ),
+                          child: const Image(
+                              image:
+                                  AssetImage('graphics/icons8-google-48.png')),
+                          onPressed: () {
+                            final provider = Provider.of<GoogleSignInProvider>(
+                                context,
+                                listen: false);
+                            provider.googleLogin();
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const BottomBar()));
+                          }),
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                        ),
+                        onPressed: () {
+                          final appleProvider =
+                              Provider.of<AppleSignInProvider>(context,
+                                  listen: false);
+                          appleProvider.appleLogin();
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const BottomBar()));
+                        },
+                        child: const Image(
+                          image:
+                              AssetImage('graphics/icons8-apple-logo-50.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
                 ),
               ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
 
