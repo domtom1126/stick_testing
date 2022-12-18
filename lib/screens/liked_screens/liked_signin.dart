942117fec5625bf0862030edfe256b5447f0d8ae
dom thomas <domtom1126@gmail.com>
2022-12-18 01:08:21 -0500

    Email/password validation works. Need to configure for rest of app

--- lib/screens/liked_screens/liked_signin.dart
+++ lib/screens/liked_screens/liked_signin.dart
@@ -13,155 +13,171 @@ class LikedSignIn extends StatelessWidget {
   LikedSignIn({
     Key? key,
   }) : super(key: key);
+  // For email validation
+  final emailRegex = RegExp(
+      r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomPaint(
-        painter: OrangeLines(),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Text(
-              'Log in to view liked cars',
-              style: TextStyle(color: HexColor('FFFFFF'), fontSize: 24),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: emailController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  hintText: 'Email',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
+    return Form(
+      key: _formKey,
+      child: Center(
+        child: CustomPaint(
+          painter: OrangeLines(),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: passwordController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  hintText: 'Password',
-                ),
-                validator: (value) {
-                  // TODO put proper validation for password
-                  if (value!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
+              Text(
+                'Log in to view liked cars',
+                style: TextStyle(color: HexColor('FFFFFF'), fontSize: 24),
+                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 250,
-              child: ElevatedButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text,
-                    );
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LikedScreen()),
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'email-already-in-use') {
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(
-                          email: emailController.text,
-                          password: passwordController.text);
-                      if (e.code == 'wrong-password') {}
+              const SizedBox(
+                height: 25,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  textInputAction: TextInputAction.next,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(20),
+                  ],
+                  controller: emailController,
+                  keyboardAppearance: Brightness.dark,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: HexColor('EE815A'), width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    hintText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email can\'t be blank';
+                    } else if (!emailRegex.hasMatch(value)) {
+                      return 'Enter valid email';
                     }
-                  }
-                },
-                child: const Text('Sign in / up'),
+                    return null;
+                  },
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  obscureText: true,
+                  textInputAction: TextInputAction.next,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(20),
+                  ],
+                  controller: passwordController,
+                  keyboardAppearance: Brightness.dark,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: HexColor('EE815A'), width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    hintText: 'Password',
+                  ),
+                  validator: (value) {
+                    // TODO put proper validation for password
+                    if (value == null || value.isEmpty) {
+                      return 'Password can\'t be blank';
+                    } else if (value.length < 8) {
+                      return 'Password must be at least 8 characters long';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LikedScreen()),
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'email-already-in-use') {
+                          await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: emailController.text,
+                                  password: passwordController.text);
+                          if (e.code == 'wrong-password') {}
+                        }
+                      }
+                    }
+                  },
+                  child: const Text('Sign in / up'),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                        ),
+                        child: const Image(
+                            image: AssetImage('graphics/icons8-google-48.png')),
+                        onPressed: () {
+                          final provider = Provider.of<GoogleSignInProvider>(
+                              context,
+                              listen: false);
+                          provider.googleLogin();
+                        }),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.transparent,
                       ),
-                      child: const Image(
-                          image: AssetImage('graphics/icons8-google-48.png')),
                       onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
+                        final appleProvider = Provider.of<AppleSignInProvider>(
                             context,
                             listen: false);
-                        provider.googleLogin();
-                      }),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                    ),
-                    onPressed: () {
-                      final appleProvider = Provider.of<AppleSignInProvider>(
-                          context,
-                          listen: false);
-                      appleProvider.appleLogin();
-                    },
-                    child: const Image(
-                      image: AssetImage('graphics/icons8-apple-logo-50.png'),
+                        appleProvider.appleLogin();
+                      },
+                      child: const Image(
+                        image: AssetImage('graphics/icons8-apple-logo-50.png'),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
