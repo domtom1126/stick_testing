6c30ad17f9657c10ee0e59774fc31f67a5ece245
dom thomas <domtom1126@gmail.com>
2022-12-18 09:05:36 -0500

    Fixed email/pass validation

--- lib/screens/post_widgets/post_sign_in.dart
+++ lib/screens/post_widgets/post_sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:find_a_stick/screens/post_widgets/post_form.dart';
 import 'package:find_a_stick/signin_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,145 +20,181 @@ class PostSignIn extends StatefulWidget {
 class _PostSignInState extends State<PostSignIn> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final emailRegex = RegExp(
+      r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return buildSignIn(context);
   }
 
-  Center buildSignIn(BuildContext context) {
-    return Center(
-      child: CustomPaint(
-        painter: OrangeLines(),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Text(
-              'Log in to post',
-              style: TextStyle(color: HexColor('FFFFFF'), fontSize: 24),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: _emailController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
+  Form buildSignIn(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Center(
+        child: CustomPaint(
+          painter: OrangeLines(),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Text(
+                'Log in to post',
+                style: TextStyle(color: HexColor('FFFFFF'), fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  textInputAction: TextInputAction.next,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(20),
+                  ],
+                  controller: _emailController,
+                  keyboardAppearance: Brightness.dark,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: HexColor('EE815A'), width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    hintText: 'Email',
                   ),
-                  hintText: 'Email',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email can\'t be blank';
+                    } else if (!emailRegex.hasMatch(value)) {
+                      return 'Enter valid email';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  return null;
-                },
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                textInputAction: TextInputAction.next,
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                ],
-                controller: _passwordController,
-                keyboardAppearance: Brightness.dark,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: HexColor('EE815A'), width: 2.0),
-                    borderRadius: BorderRadius.circular(25.0),
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  obscureText: true,
+                  textInputAction: TextInputAction.done,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(20),
+                  ],
+                  controller: _passwordController,
+                  keyboardAppearance: Brightness.dark,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: HexColor('EE815A'), width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    hintText: 'Password',
                   ),
-                  hintText: 'Password',
+                  validator: (value) {
+                    // TODO put proper validation for password
+                    if (value == null || value.isEmpty) {
+                      return 'Password can\'t be blank';
+                    } else if (value.length < 8) {
+                      return 'Password must be at least 8 characters long';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  // TODO put proper validation for password
-                  if (value!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 250,
-              child: ElevatedButton(
-                onPressed: () {
-                  signInUp(
-                    _emailController.toString(),
-                    _passwordController.toString(),
-                  );
-                  // TODO Update to the signed in page
-                },
-                child: Text('Sign in / up'),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
+              SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      );
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const PostForm()),
+                      );
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'email-already-in-use') {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: _passwordController.text);
+                      } else if (e.code == 'wrong-password') {
+                        setState(() {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const AlertDialog(
+                                    backgroundColor: Colors.black45,
+                                    title: Text('Privacy Policy'),
+                                    content: Text('hello'));
+                              });
+                        });
+                      }
+                    }
+                  },
+                  child: Text('Sign in / up'),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                        ),
+                        child: const Image(
+                            image: AssetImage('graphics/icons8-google-48.png')),
+                        onPressed: () {
+                          final provider = Provider.of<GoogleSignInProvider>(
+                              context,
+                              listen: false);
+                          provider.googleLogin();
+                        }),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.transparent,
                       ),
-                      child: const Image(
-                          image: AssetImage('graphics/icons8-google-48.png')),
                       onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
+                        final appleProvider = Provider.of<AppleSignInProvider>(
                             context,
                             listen: false);
-                        provider.googleLogin();
-                      }),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                    ),
-                    onPressed: () {
-                      final appleProvider = Provider.of<AppleSignInProvider>(
-                          context,
-                          listen: false);
-                      appleProvider.appleLogin();
-                    },
-                    child: const Image(
-                      image: AssetImage('graphics/icons8-apple-logo-50.png'),
+                        appleProvider.appleLogin();
+                      },
+                      child: const Image(
+                        image: AssetImage('graphics/icons8-apple-logo-50.png'),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -168,9 +205,9 @@ class OrangeLines extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
-      ..color = HexColor('EE6C4D')
+      ..color = HexColor('DF7212')
       ..strokeWidth = 15
-      ..strokeCap = StrokeCap.round;
+      ..strokeCap = StrokeCap.square;
 
     canvas.drawLine(const Offset(200, -125), const Offset(550, 200), paint);
 
