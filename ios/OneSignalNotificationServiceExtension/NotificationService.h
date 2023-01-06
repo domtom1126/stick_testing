0761c22fdc839277e79f303c29f0f1438a11b169
dom thomas <domtom1126@gmail.com>
2023-01-05 23:39:29 -0500

    Updating...

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Dominic . on 1/4/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
